82551b4fbd156c5cdc4eba58783548d045c1eb55
Muhammad Faaiz Sheikh <147300364+Faaizshaikh@users.noreply.github.com>
2025-09-19 13:28:43 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,12 @@ const taskRoutes = require('./routes/tasks');
 
 const app = express();
 app.use(bodyParser.json());
-
-// Routes
 app.use('/api/tasks', taskRoutes);
 
-// Export app for testing
+// Export app for Supertest
 module.exports = app;
 
-// Start server only if not in test mode
 if (require.main === module) {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+  app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
 }
